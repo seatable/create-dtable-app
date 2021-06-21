3ff92785e008abde5643c6e4330534d6071b8402
shanshuirenjia <978987373@qq.com>
2021-06-21 11:38:14 +0800

    init project

---
+++ bin/index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const process1 = require('child_process');
+const ora = require('ora');
+const rm = require('rimraf').sync
+const execSync = require('child_process').execSync;
+
+const spinner = ora();
+const gitUrl = 'https://github.com/seatable/seatable-app-template.git';
+
+program
+  .version(require('../package').version, '-v, --version')
+  .command('init <name>')
+  .action((name) => {
+      console.log('start loading template');
+      const projectName = name;
+      spinner.start('clone template ...');
+      process1.exec('git clone ' + gitUrl + ' ' + './' + projectName, function(error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error);
+          return;
+        }
+        rm('./' + projectName + '/.git');
+        console.log(stdout);
+        spinner.stop();
+        console.log('loading template success');
+      });
+  });
+program.parse(process.argv);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# dtable app 开发脚手架
+
+# 使用步骤
+
+1. 全局安装脚手架
+  `npm install create-dtable-app -g`
+
+2. 创建插件项目
+  `create-dtable-app init <plugin-name>`
+
+
+注：该插件为简版插件，后续会添加一些自动的检测功能及交互功能
+* 创建项目时，请避免当前目录下有同名文件夹
