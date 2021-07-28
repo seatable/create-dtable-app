c3ac4b3ae0d3bc483dd166554f13e0efa60dea23
shanshuirenjia <978987373@qq.com>
2021-07-28 13:35:19 +0800

    update script

--- bin/index.js
+++ bin/index.js
@@ -13,18 +13,26 @@ program
   .version(require('../package').version, '-v, --version')
   .command('init <name>')
   .action((name) => {
-      console.log('start loading template');
-      const projectName = name;
-      spinner.start('clone template ...');
-      process1.exec('git clone ' + gitUrl + ' ' + './' + projectName, function(error, stdout, stderr) {
-        if (error !== null) {
-          console.log('exec error: ' + error);
-          return;
-        }
-        rm('./' + projectName + '/.git');
-        console.log(stdout);
-        spinner.stop();
-        console.log('loading template success');
+    console.log('start loading template');
+    const projectName = name;
+    spinner.start('clone template ...');
+    process1.exec('git clone ' + gitUrl + ' ' + './' + projectName, function(error, stdout, stderr) {
+      if (error !== null) {
+        console.log('exec error: ' + error);
+        return;
+      }
+      rm('./' + projectName + '/.git');
+
+      const cwd = process.cwd();
+      const context = path.resolve(cwd, projectName);
+
+      execSync('git init', { stdio: 'ignore', cwd: context });
+      execSync('git add .', { stdio: 'ignore', cwd: context });
+      execSync('git commit -m "Initialize project using create-dtable-app"', {stdio: 'ignore', cwd: context});
+      
+      console.log(stdout);
+      spinner.stop();
+      console.log('loading template success');
       });
   });
 program.parse(process.argv);
